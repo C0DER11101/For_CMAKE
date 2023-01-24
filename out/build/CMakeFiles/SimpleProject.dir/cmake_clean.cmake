b4b81f8c4699868580dabae98701d66846a76229
Priyanuj Bora <while.alive.program@gmail.com>
2023-01-24 18:48:12 +0530

    Part of tutorial

---
+++ out/build/CMakeFiles/SimpleProject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SimpleProject.dir/main.c.o"
+  "SimpleProject"
+  "SimpleProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SimpleProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
