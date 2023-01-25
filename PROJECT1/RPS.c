1f6dfb9dc1418fb5d9402cd73c6c4b672dfd6874
Priyanuj Bora <while.alive.program@gmail.com>
2023-01-25 20:46:36 +0530

    Mini Project

---
+++ PROJECT1/RPS.c
@@ -0,0 +1,108 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<time.h>
+#include<string.h>
+
+int main(void)
+{
+	srand(time(NULL));
+	char *playerName=(char*)malloc(100*sizeof(char));
+	unsigned int CompScore=0, PlayerScore=0, chance=6;
+	char choice, compChoice; // for player and the computer!!
+	const char CompChoice[]={'r', 'p', 's'};
+
+	printf("\n\t\t**WELCOME TO ROCK-PAPER-SCISSOR\t\t");
+	printf("\nGame instructions:\n");
+	printf("\nPress \"R\" for rock, \"P\" for paper and \"S\" for scissor\n");
+	printf("Please enter your name: ");
+	scanf("%s", playerName);
+
+	printf("\nStarting the game...\n");
+
+	system("sleep 1");
+	system("clear");
+
+	while(chance)
+	{
+		printf("%s, press your key!\n", playerName);
+		scanf(" %c", &choice);
+
+		printf("\nYou chose: ");
+		if(tolower(choice)=='r')
+			printf("Rock\n");
+
+		else if(tolower(choice)=='p')
+			printf("Paper\n");
+
+		else if(tolower(choice)=='s')
+			printf("Scissor\n");
+		else
+		{
+			printf("Invalid input!!Enter again!!\n");
+			continue;
+		}
+
+		compChoice=CompChoice[0+(rand()%3)];
+		printf("\nI chose: ");
+
+		if(tolower(compChoice)=='r')
+			printf("Rock\n");
+
+		else if(tolower(compChoice)=='p')
+			printf("Paper\n");
+
+		else if(tolower(compChoice)=='s')
+			printf("Scissor\n");
+
+
+		if(tolower(choice)=='r' && tolower(compChoice)=='s')
+		{
+			PlayerScore=PlayerScore*2+1;
+		}
+		else if(tolower(choice)=='s' && tolower(compChoice)=='r')
+		{
+			CompScore=CompScore*2+1;
+			chance-=1;
+			printf("\nYou have got %d chance(s) left!!\n", chance);
+		}
+		else if(tolower(choice)=='r' && tolower(compChoice)=='p')
+		{
+			CompScore=CompScore*2+1;
+			chance-=1;
+			printf("\nYou have got %d chance(s) left!!\n", chance);
+		}
+		else if(tolower(choice)=='p' && tolower(compChoice)=='r')
+		{
+			PlayerScore=PlayerScore*2+1;
+		}
+		else if(tolower(choice)=='s' && tolower(compChoice)=='p')
+		{
+			PlayerScore=PlayerScore*2+1;
+		}
+		else if(tolower(choice)=='p' && tolower(compChoice)=='s')
+		{
+			CompScore=CompScore*2+1;
+			chance-=1;
+			printf("\nYou have got %d chance(s) left!!\n", chance);
+		}
+
+		system("sleep 3");
+		system("clear");
+
+	}
+
+	printf("%s's score: %d\n", playerName, PlayerScore);
+	printf("My score: %d\n", CompScore);
+
+	if(PlayerScore>CompScore)
+		printf("\nCongratulations %s! You won the game!!\n", playerName);
+	else if(CompScore>PlayerScore)
+		printf("\nI won the game!!\n");
+	else
+		printf("\nIt's a draw!!\n");
+
+	free(playerName);
+
+	return 0;
+}
---
+++ PROJECT1/out/build/CMakeFiles/RPS.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RPS.dir/RPS.c.o"
+  "RPS"
+  "RPS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/RPS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
